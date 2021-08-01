42f0b02a4967d86af3b63402a52d387afd491184
KangMamles <imzea69@gmail.com>
2021-08-01 12:40:22 +0000

    Import GCC 11.2.0, Binutils 2.37
    Signed-off-by: KangMamles <imzea69@gmail.com>

---
+++ lib/gcc/arm-linux-gnueabi/11.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/runner/work/gcc-build/gcc-build/gcc/configure --target=arm-linux-gnueabi --enable-targets=all --prefix=/home/runner/work/gcc-build/gcc-build/arm-gcc --enable-languages=c --without-headers --disable-bootstrap --disable-nls --disable-threads --disable-shared --disable-libmudflap --disable-libssp --disable-libgomp --disable-decimal-float --disable-libquadmath --disable-libatomic --disable-libcc1 --disable-libmpx --enable-checking=release --with-newlib --with-zstd=/home/runner/work/gcc-build/gcc-build/build-zstd/install";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "arm10tdmi" }, { "tls", "gnu" } };
